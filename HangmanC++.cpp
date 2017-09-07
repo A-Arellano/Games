84663b6f98cd7127a3fa5d6ca09d7dc015c4d92e
A-Arellano <krollarellano@gmail.com>
2017-09-07 17:46:55 -0300

    Mejoras y problema del array

--- HangmanC++.cpp
+++ HangmanC++.cpp
@@ -5,18 +5,19 @@ using namespace std;
 
 void pedirletra(int &intentos_restantes, string palabra, string respuesta);
 string llenar_respuesta (string palabra);
+void letras_usadas (char letra, int &usos, array letras[]);
 
 int main()
 {
     string palabra;
     string respuesta;
-
     int intentos_restantes = 5;
     cout << "Palabra" << endl;
     cin >> palabra;
     respuesta = llenar_respuesta(palabra);
     system("cls");
     pedirletra(intentos_restantes, palabra, respuesta);
+
     return 0;
 }
 
@@ -25,6 +26,13 @@ void pedirletra(int &intentos_restantes, string palabra, string respuesta)
 
     char letra;
     bool conf;
+    int usos;
+    string fin = palabra;
+    array letras[27];
+    for (int i = 0 ; i < 27 ; i++)
+    {
+        letras[i] = 0;
+    }
     while (intentos_restantes > 0)
     {
         system("cls");
@@ -33,7 +41,7 @@ void pedirletra(int &intentos_restantes, string palabra, string respuesta)
         conf = false;
         cout << "Inserte una letra - Intentos restantes: " << intentos_restantes << endl;
         cin >> letra;
-
+        letras_usadas(letra,usos,letras);
         for (int i = 0; i < palabra.size(); i++) //Comprobar existencia de la letra en la palabra
             {
               if (letra == palabra[i])
@@ -54,6 +62,17 @@ void pedirletra(int &intentos_restantes, string palabra, string respuesta)
                 system("pause");
                 intentos_restantes--;
             }
+            if (respuesta == fin)
+            {
+                system("cls");
+                cout << "Ganaste!" << endl;
+                intentos_restantes = 0;
+            }
+    }
+    if (respuesta != fin)
+    {
+        system("cls");
+        cout << "Perdiste mongo" << endl;
     }
 }
 
@@ -66,3 +85,11 @@ string llenar_respuesta (string palabra)
 }
 
 
+void letras_usadas (char letra, int &usos, array letras[])
+{
+    for (usos ; usos < 27 ; usos++)
+    {
+        letras[usos = letra];
+    }
+
+}
